c5ae809cb095f0c582ce5ada80102e8417f780ff
YEYE <63726895+Thasada@users.noreply.github.com>
2023-11-20 04:14:36 +0700

    Update user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const authJwt = require("../middlewares/auth.jwt");
   const user_controller = require("../controllers/user.controller");
   var router = require("express").Router();
-  //router.get("/:us", user_controller.validUsername);
+  router.get("/:us", user_controller.validUsername);
   router.post("/signup", user_controller.createNewUser);
   router.post("/login", user_controller.login);
   router.get("/", authJwt, user_controller.getAllUsers);
